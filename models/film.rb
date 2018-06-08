2a269fd6d56ee8698e231305338682f8b368dc82
helenosheaa <helenosheaa@gmail.com>
2018-06-08 11:37:58 +0100

    update method film

--- models/film.rb
+++ models/film.rb
@@ -20,6 +20,13 @@ def save ()
   @id = results[0]['id'].to_i()
 end
 
+def update()
+  sql = "UPDATE films SET (title, price)
+  = ($1, $2) WHERE id = $3"
+  values = [@title, @price, @id]
+  SqlRunner.run(sql, values)
+end
+
 def self.all()
   sql = "SELECT * FROM films"
   films = SqlRunner.run(sql)
