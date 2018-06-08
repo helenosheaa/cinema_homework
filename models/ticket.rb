ed97669af4c34a4bcb20fe6edb2ba8c0f8a6146a
helenosheaa <helenosheaa@gmail.com>
2018-06-08 11:40:26 +0100

    update method tickets

--- models/ticket.rb
+++ models/ticket.rb
@@ -20,6 +20,13 @@ def save ()
   @id = results[0]['id'].to_i()
 end
 
+def update()
+  sql = "UPDATE tickets SET (customer_id, film_id)
+  = ($1, $2) WHERE id = $3"
+  values = [@customer_id, @film_id, @id]
+  SqlRunner.run(sql, values)
+end
+
 def self.all()
   sql = "SELECT * FROM tickets"
   tickets = SqlRunner.run(sql)
